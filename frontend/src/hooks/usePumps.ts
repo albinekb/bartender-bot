0488c76ef644ca81eae10514351d76bc3f2537ae
Albin Ekblom <albinekblom@gmail.com>
2023-09-02 16:58:54 +0200

    Update frontend

---
+++ frontend/src/hooks/usePumps.ts
@@ -0,0 +1,16 @@
+export function usePumps() {
+  return [
+    {
+      id: 'test',
+      name: 'Test',
+    },
+    {
+      id: 'test 2',
+      name: 'Test 2',
+    },
+    {
+      id: 'test 3',
+      name: 'test 4',
+    },
+  ]
+}
